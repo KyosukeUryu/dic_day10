b53229516683aca9cd9001ef4333e4a0de81a160
yu shimamura <mashiyu0914@gmail.com>
2019-12-25 18:31:24 +0900

    時間切れ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+    resources :blogs, only: [:index, :new, :create, :edit, :update, :show]
+    resources :users, only: [:new, :create, :edit, :update, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
