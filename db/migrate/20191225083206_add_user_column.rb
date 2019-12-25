98db79850d6b4ab87f3e5eb371d36f6be4e5a3a7
kyosuke uryu <kyo.uryu33@gmail.com>
2019-12-25 17:49:23 +0900

    [add]userのパスワード追加

--- db/migrate/20191225083206_add_user_column.rb
+++ db/migrate/20191225083206_add_user_column.rb
@@ -1,5 +1,5 @@
 class AddUserColumn < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :password_digest, null: false
+    add_column :users, :password_digest, :string, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_25_082214) do
+ActiveRecord::Schema.define(version: 2019_12_25_083206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_12_25_082214) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest", null: false
   end
 
 end
