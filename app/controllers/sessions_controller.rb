9595e27a708f6b85b1c27615b37d86f516dc409b
kyosuke uryu <kyo.uryu33@gmail.com>
2019-12-25 18:10:46 +0900

    [add]最低限のview作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,10 +11,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to tasks_path, notice: 'ログインしました'
+      redirect_to root_path, notice: 'ログインしました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render
+    end
   end
 
   def destroy
